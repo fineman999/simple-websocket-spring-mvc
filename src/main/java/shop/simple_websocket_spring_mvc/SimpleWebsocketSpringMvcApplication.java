818fec621adddf4e176dc5a93110734598e20897
Chan <33cks1423@naver.com>
2023-10-07 15:05:17 +0900

    feat(init): project setup

---
+++ src/main/java/shop/simple_websocket_spring_mvc/SimpleWebsocketSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package shop.simple_websocket_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebsocketSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebsocketSpringMvcApplication.class, args);
+    }
+
+}
