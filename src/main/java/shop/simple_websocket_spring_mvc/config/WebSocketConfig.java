2ca48a6268101676d1537761a4d55b66f299fe33
Chan <33cks1423@naver.com>
2023-10-07 16:05:10 +0900

    feat(websocket configure): all origin

--- src/main/java/shop/simple_websocket_spring_mvc/config/WebSocketConfig.java
+++ src/main/java/shop/simple_websocket_spring_mvc/config/WebSocketConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
@@ -13,11 +15,22 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(myHandler(), "/myHandler");
+        registry.addHandler(myHandler(), "/myHandler")
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                .setAllowedOrigins("*");
     }
 
     @Bean
     public WebSocketHandler myHandler() {
         return new MyHandler();
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container =
+                new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(8192);
+        container.setMaxBinaryMessageBufferSize(8192);
+        return container;
     }
 }
