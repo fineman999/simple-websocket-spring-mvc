e5ab8753410c05d8ed8efa2dbc5ee6203d578ed3
Chan <33cks1423@naver.com>
2023-10-08 12:46:20 +0900

    feat(STOMP): Performance

--- src/main/java/shop/simple_websocket_spring_mvc/config/WebSocketConfigV2.java
+++ src/main/java/shop/simple_websocket_spring_mvc/config/WebSocketConfigV2.java
@@ -6,6 +6,8 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfigV2 implements WebSocketMessageBrokerConfigurer {
@@ -28,5 +30,8 @@ public class WebSocketConfigV2 implements WebSocketMessageBrokerConfigurer {
         registration.interceptors(new MyChannelInterceptor());
     }
 
-
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+        registry.setSendTimeLimit(128 * 1024).setSendBufferSizeLimit(512 * 1024).setMessageSizeLimit(128 * 1024);
+    }
 }
